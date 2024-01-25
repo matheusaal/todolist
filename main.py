9362db0e3992be56c5a9ecb0dadd7c7b7b10c15a
matheusaal <matheus.avila31@gmail.com>
2024-01-25 12:20:57 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+class Interface():
+
+    def menu(self):
+
+        print("Option 1: Create a new task")
+        print("Option 2: Delete a task")
+        print("Option 3: Create a new task")
+        
